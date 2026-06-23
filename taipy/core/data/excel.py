65d2a88c57b11017bce262454e76eb8fa55bc201
xcr <xcr@xcr>
2026-06-23 22:06:52 +0800

    Kusanagi238__diff__id_135

--- taipy/core/data/excel.py
+++ taipy/core/data/excel.py
@@ -176,9 +176,14 @@ class ExcelDataNode(DataNode, _FileDataNodeMixin, _TabularDataNodeMixin):
             work_books = {}
             sheet_names = excel_file.sheetnames
 
-            user_provided_sheet_names = properties.get(self.__SHEET_NAME_PROPERTY) or []
-            if not isinstance(user_provided_sheet_names, (list, set, tuple)):
-                user_provided_sheet_names = [user_provided_sheet_names]
+            user_provided_sheet_names = properties.get(self.__SHEET_NAME_PROPERTY)
+            if user_provided_sheet_names is None:
+                user_provided_sheet_names = []
+            elif not isinstance(user_provided_sheet_names, list):
+                if isinstance(user_provided_sheet_names, (set, tuple)):
+                    user_provided_sheet_names = list(user_provided_sheet_names)
+                else:
+                    user_provided_sheet_names = [user_provided_sheet_names]
 
             provided_sheet_names = user_provided_sheet_names or sheet_names
 
--- taipy/gui/page.py
+++ taipy/gui/page.py
@@ -76,7 +76,10 @@ class Page:
         # Special variables only use for page reloading in notebook context
         self._notebook_gui: t.Optional["Gui"] = None
         self._notebook_page: t.Optional["_Page"] = None
-        self.set_style(kwargs.get("style", None))
+        style = kwargs.get("style", None)
+        if style is not None and not isinstance(style, dict):
+            style = None
+        self.set_style(t.cast(t.Optional[t.Dict[str, t.Dict[str, t.Any]]], style))
         self._script_paths(kwargs.get("script_paths", None))
 
     def create_page(self) -> t.Union[Page, str, None]:
